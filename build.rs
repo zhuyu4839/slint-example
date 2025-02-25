67612c176f05c8856603eb235298f30dcc02377a
blockish <blockish@yeah.net>
2025-02-25 10:27:31 +0800

    feat: init commit for ui

---
+++ build.rs
@@ -0,0 +1,8 @@
+
+fn main() -> anyhow::Result<()> {
+    let config = slint_build::CompilerConfiguration::new()
+        .with_bundled_translations("lang");
+    slint_build::compile_with_config("ui/main.slint", config)?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+slint::include_modules!();
+
+fn main() -> anyhow::Result<()> {
+    let app = AppWindow::new()?;
+
+    // app.on_request_increase_value({
+    //     let ui_handle = app.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         ui.set_counter(ui.get_counter() + 1);
+    //     }
+    // });
+
+    app.run()?;
+
+    Ok(())
+}
